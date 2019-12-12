ff81181affeb90259b6a627212fdb6a2beeeb157
yourtechsis <aretee64@gmail.com>
2019-12-12 01:48:58 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+const Food = () => {
+  return (
+    <div>
+      <p> foooood page</p>
+    </div>
+  )};
+
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/food' component={Food} />
+      </Switch >
     </div>
   );
 }
--- src/components/menu-item/menu-items.component.jsx
+++ src/components/menu-item/menu-items.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import './menu-items.styles.scss'
 
-const MenuItem = ({ title, url }) => (
-
-    <div style={{
+const MenuItem = ({ title, url, history, link, match }) => (
+<div className='food-item' onClick={() =>history.push(`${match.url}${link}`)} >
+    <div className='background-image' style={{
         backgroundImage: `url(${url})`
-    }}
-        className="food-item">
+    }}>
         <div className="food-content">
             <h1 className="title">{title}</h1>
             <span>Price $</span>
         </div>
-    </div>
+    </div></div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
+// give access to history props from Route
